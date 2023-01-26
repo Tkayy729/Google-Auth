a37d25171e300f0d3628d03229aa0245bfbc33d9
Emmanuel Tweneboah <emmanuel.tweneboah@turntabl.io>
2023-01-26 10:59:29 +0000

    google auth

---
+++ Downloads/Oauth-authorization-server/Oauth-authorization-server/src/main/java/com/tkayycode/oauthserver/dto/CustomerData.java
@@ -0,0 +1,19 @@
+package com.tkayycode.oauthserver.dto;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+public class CustomerData {
+
+    private Map<String,Integer> customerData = Stream.of(new Object[][]{
+            {"AMIT DATTA", 1000}
+    }).collect(Collectors.toMap(data -> (String) data[0],data-> (Integer) data[1]));
+
+    public Integer getBalance(String customerName){
+        return customerData.get(customerName);
+    }
+}
---
+++ Downloads/Oauth-authorization-server/Oauth-authorization-server/src/main/java/com/tkayycode/oauthserver/securityConfig/WebSecurity.java
@@ -0,0 +1,20 @@
+package com.tkayycode.oauthserver.securityConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+              System.out.println("Bean created.................");
+                 httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and().oauth2Login();
+
+        return httpSecurity.build();
+    }
+}
